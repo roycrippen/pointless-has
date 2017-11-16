82ecc9b6785c6531814ae29787e5e2234086ed1d
roycrippen <crippenre@ss-crippenr-lnx.ds.amrdec.army.mil>
2017-11-16 14:55:01 -0600

    cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,7 @@
 module Main where
 
 import qualified Data.Map        as Map
-import           Interpreter     (Stack, Value (..), WordP (..), formatStack,
-                                  runQuotation)
+import           Interpreter     (Stack, WordP (..), formatStack, runQuotation)
 import           Parser
 import           PointlessParser (program)
 import           Primitives      (primitives)
@@ -12,12 +11,6 @@ getProgram = do
     source <- readFile "data/test.joy"
     let ((definitions, quotations), _) = head $ parse program source
     return  (primitives ++ definitions, quotations)
-
-stackT :: Stack
-stackT = [Quot [Number 1.0,Number 2.0],Number 3.0]
-
-sourceT :: String
-sourceT = " (*start*)  DEFINE dup' == dup ; DEFINE pop' == pop ; # aaa \n  # bbb \n (*ccc*) # ddd \n DEFINE roy' == roy ; (*eee*)"
 
 
 main :: IO ()
