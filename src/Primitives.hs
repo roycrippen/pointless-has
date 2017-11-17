f4b3e2af872c7591b6fc649a9354bf2b6634d6ea
roycrippen <crippenre@ss-crippenr-lnx.ds.amrdec.army.mil>
2017-11-17 13:27:20 -0600

    cleanup

--- src/Primitives.hs
+++ src/Primitives.hs
@@ -3,8 +3,6 @@ module Primitives where
 import           Interpreter (Lang (..), Value (..), WordP (..), formatV,
                               isTrue, runQuotation, toTruth)
 
--- import           Debug.Trace
-
 -- Primitives
 pop :: Lang -> Lang
 pop lang = case stack lang of
@@ -61,17 +59,17 @@ ifThenElse lang = case stack lang of
       where (result:_) = stack $ runQuotation qif (lang { stack = cs })
     _ -> lang { errors = "ifte: three quotations expected" : errors lang }
 
-arith :: (Double -> Double -> Double)->Lang -> Lang
+arith :: (Double -> Double -> Double) -> Lang -> Lang
 arith operator lang = case (operator, stack lang) of
     (op, (Number y:(Number c:cs)))  -> lang { stack = Number (op c y) : cs }
     (_, _)                          -> lang { errors = "arithmetic operation: two numbers expected" : errors lang }
 
-comparison :: (Double -> Double -> Bool)-> Lang -> Lang
+comparison :: (Double -> Double -> Bool) -> Lang -> Lang
 comparison operator lang = case (operator, stack lang) of
     (op, (Number y:(Number c:cs)))  -> lang { stack = toTruth (op c y) : cs }
     (_, _)                          -> lang { errors = "comparison operation: two numbers expected" : errors lang }
 
-logic :: (Bool -> Bool -> Bool)->Lang -> Lang
+logic :: (Bool -> Bool -> Bool) -> Lang -> Lang
 logic operator lang = case (operator, stack lang) of
     (op, (y:c:cs))  -> lang { stack = toTruth (op (isTrue c) (isTrue y)) : cs }
     (_, _)          -> lang { errors = "logic operation: two values expected" : errors lang }
@@ -89,72 +87,6 @@ unstack :: Lang -> Lang
 unstack lang = case stack lang of
     (Quot ys:_)-> lang { stack = ys }
     _      -> lang { errors = "unstack: quotation expected" : errors lang }
-
--- pop :: Stack -> Stack
--- pop (_:cs) = cs
--- pop _      = error "pop: stack underflow"
-
--- dup :: Stack -> Stack
--- dup (c:cs) = c : c : cs
--- dup _      = error "dup: stack empty"
-
--- dip :: Vocabulary -> Stack -> Stack
--- dip vocab (Quot q:(c:cs)) = c : runQuotation q vocab cs
--- dip _     _               = error "dip: value and quotation expected"
-
--- x :: Vocabulary -> Stack -> Stack
--- x vocab (Quot q:cs) = runQuotation q vocab [] ++ [Quot q] ++ cs
--- x _     _           = error "x: quotation must be executable without a stack"
-
--- i :: Vocabulary -> Stack -> Stack
--- i vocab (Quot q:cs) = runQuotation q vocab cs
--- i _     _           = error "x: quotation must be executable without a stack"
-
--- cons :: Stack -> Stack
--- cons (Quot q:(c:cs)) = Quot (c : q) : cs
--- cons _               = error "cons: value and quotation expected"
-
--- uncons :: Stack -> Stack
--- uncons (Quot (c:is):cs) = Quot is : c : cs
--- uncons _                = error "uncons: quotation with at least one element expected"
-
--- concatP :: Stack -> Stack
--- concatP (Quot s:(Quot t:cs)) = Quot (t ++ s) : cs
--- concatP _                    = error "concatP: two quotations expected"
-
--- printVal :: Stack -> Stack
--- printVal (_:cs) = cs
--- printVal _      = error "printVal: stack empty"
-
--- ifThenElse :: Vocabulary -> Stack -> Stack
--- ifThenElse vocab (Quot qelse:(Quot qthen:(Quot qif:cs))) = if isTrue result
---     then runQuotation qthen vocab cs
---     else runQuotation qelse vocab cs
---     where (result:_) = runQuotation qif vocab cs
--- ifThenElse _ _ = error "ifte: three quotations expected"
-
--- arith :: (Double -> Double -> Double) -> Stack -> Stack
--- arith op (Number y:(Number c:cs)) = Number (op c y) : cs
--- arith _ _                         = error "arithmetic operation: two numbers expected"
-
--- comparison :: (Double -> Double -> Bool) -> Stack -> Stack
--- comparison op (Number y:(Number c:cs)) = toTruth (op c y) : cs
--- comparison _ _                         = error "comparison operation: two numbers expected"
-
--- logic :: (Bool -> Bool -> Bool) -> Stack -> Stack
--- logic op (y:c:cs) = toTruth (op (isTrue c) (isTrue y)) : cs
--- logic _  _        = error "logic operation: two values expected"
-
--- lnot :: Stack -> Stack
--- lnot (c:cs) = toTruth (not (isTrue c)) : cs
--- lnot _      = error "null: value expected"
-
--- stackP :: Stack -> Stack
--- stackP cs = Quot cs : cs
-
--- unstack :: Stack -> Stack
--- unstack (Quot ys:_) = ys
--- unstack _           = error "unstack: quotation expected"
 
 truncMod :: (RealFrac a, RealFrac a1) => a1 -> a -> Double
 truncMod c y = fromInteger (truncate c `mod` truncate y) :: Double
