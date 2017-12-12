a4f11511e70753513584ac25c2b55164e7c79266
roycrippen <crippenre@ss-crippenr-lnx.ds.amrdec.army.mil>
2017-12-12 14:45:27 -0600

    parse newlines in quoted string

--- test/Spec.hs
+++ test/Spec.hs
@@ -17,11 +17,8 @@ import           Test.Tasty.HUnit
 sKeep01 :: String
 sKeep01 = "\"aaa\" [1.1] def aaa aaa [] cons cons dup "
 
-s02 :: String
-s02 = "\"a\nb\" putchars ."
-
-s03 :: String
-s03 = "\"a\n\n\nb\" . "
+sKeep02 :: String
+sKeep02 = "\"a\\\n\\\nz\" putchars ."
 
 runQuot :: String -> Lang
 runQuot s = runQuotation qs lang
@@ -33,199 +30,120 @@ runQuot s = runQuotation qs lang
 main :: IO ()
 main = do
 
-    let (qs, _) = head $ parse nakedQuotations s02
-    mapM_ (putStrLn . formatV) qs
+  let (qs, _) = head $ parse nakedQuotations sKeep02
+      xs = map formatV qs
+  print xs
 
-    let res = runQuot s02
+  let res = runQuot sKeep02
 
-    putStrLn "\nqs after: = "
-    print $ stack res
+  putStrLn "\nafter result: = "
+  print (result res)
 
-    putStrLn "\nformatStack res: = "
-    print $ formatStack $ stack res
+  putStrLn "\njson : "
+  T.putStr $ jsonResultsShow res
+  putStrLn "\n"
 
-    -- putStrLn "\nvocab:"
-    -- let xs = M.toList (vocab lang)
-    -- mapM_ print xs
-
-    putStrLn "\njson : "
-    T.putStr $ jsonResultsShow res
-    putStrLn "\n"
-
-    defaultMain unitTests
+  defaultMain unitTests
 
 unitTests :: TestTree
 unitTests = testGroup
-    "Pointlees interprter tests"
-    [ parsePositiveDouble1
-    , parsePositiveDouble2
-    , parseNegativeDouble1
-    , parseNegativeDouble2
-    , parseNumberP1
-    , parseNumberP2
-    , parseNakedQuotation1
-    , parseNakedQuotation2
-    , parseNakedQuotation3
-    , parseCharP1
-    , parseCharP2
-    , parseQuotedStringP1
-    , parseQuotedStringP2
-    , formatStack1
-    ]
+  "Pointlees interprter tests"
+  [ parsePositiveDouble1
+  , parsePositiveDouble2
+  , parseNegativeDouble1
+  , parseNegativeDouble2
+  , parseNumberP1
+  , parseNumberP2
+  , parseNakedQuotation1
+  , parseNakedQuotation2
+  , parseNakedQuotation3
+  , parseCharP1
+  , parseCharP2
+  , parseQuotedStringP1
+  , parseQuotedStringP2
+  , formatStack1
+  , escapeNewLine1
+  ]
 
 parsePositiveDouble1 :: TestTree
 parsePositiveDouble1 = testCase "parse numberDouble 23"
-    $ assertEqual [] 23.0 val
-    where (val, _) = head $ parse numberDouble "23"
+  $ assertEqual [] 23.0 val
+  where (val, _) = head $ parse numberDouble "23"
 
 parsePositiveDouble2 :: TestTree
 parsePositiveDouble2 = testCase "parse numberDouble 23.4"
-    $ assertEqual [] 23.4 val
-    where (val, _) = head $ parse numberDouble "23.4"
+  $ assertEqual [] 23.4 val
+  where (val, _) = head $ parse numberDouble "23.4"
 
 parseNegativeDouble1 :: TestTree
 parseNegativeDouble1 = testCase "parse numberDouble -23"
-    $ assertEqual [] (-23.0) val
-    where (val, _) = head $ parse numberDouble "-23"
+  $ assertEqual [] (-23.0) val
+  where (val, _) = head $ parse numberDouble "-23"
 
 parseNegativeDouble2 :: TestTree
 parseNegativeDouble2 = testCase "parse numberDouble -23.4"
-    $ assertEqual [] (-23.4) val
-    where (val, _) = head $ parse numberDouble "-23.4"
-
+  $ assertEqual [] (-23.4) val
+  where (val, _) = head $ parse numberDouble "-23.4"
 
 parseNumberP1 :: TestTree
 parseNumberP1 = testCase "parse numberP -23"
-    $ assertEqual [] (NumP (-23.0)) val
-    where (val, _) = head $ parse numberP "-23"
+  $ assertEqual [] (NumP (-23.0)) val
+  where (val, _) = head $ parse numberP "-23"
 
 parseNumberP2 :: TestTree
 parseNumberP2 = testCase "parse numberP -23.4"
-    $ assertEqual [] (NumP (-23.4)) val
-    where (val, _) = head $ parse numberP "-23.4"
+  $ assertEqual [] (NumP (-23.4)) val
+  where (val, _) = head $ parse numberP "-23.4"
 
 parseNakedQuotation1 :: TestTree
 parseNakedQuotation1 =
-    testCase "parse nakedQuotaions \"-10 10 +\" "
-        $ assertEqual [] [NumP (-10.0), NumP 10.0, Symbol "+"] val
-    where (val, _) = head $ parse nakedQuotations "-10 10 +"
+  testCase "parse nakedQuotaions \"-10 10 +\" "
+    $ assertEqual [] [NumP (-10.0), NumP 10.0, Symbol "+"] val
+  where (val, _) = head $ parse nakedQuotations "-10 10 +"
 
 parseNakedQuotation2 :: TestTree
 parseNakedQuotation2 =
-    testCase "parse nakedQuotaions \"-10 'a'\" "
-        $ assertEqual [] [NumP (-10.0), Chr 'a'] val
-    where (val, _) = head $ parse nakedQuotations "-10 'a'"
+  testCase "parse nakedQuotaions \"-10 'a'\" "
+    $ assertEqual [] [NumP (-10.0), Chr 'a'] val
+  where (val, _) = head $ parse nakedQuotations "-10 'a'"
 
 parseNakedQuotation3 :: TestTree
 parseNakedQuotation3 =
-    testCase "parse nakedQuotaions \"'a' [dup 'z'] i 'b'\" " $ assertEqual
-        []
-        [Chr 'a', Quot [Symbol "dup", Chr 'z'], Symbol "i", Chr 'b']
-        val
-    where (val, _) = head $ parse nakedQuotations "'a' [dup 'z'] i 'b'"
+  testCase "parse nakedQuotaions \"'a' [dup 'z'] i 'b'\" " $ assertEqual
+    []
+    [Chr 'a', Quot [Symbol "dup", Chr 'z'], Symbol "i", Chr 'b']
+    val
+  where (val, _) = head $ parse nakedQuotations "'a' [dup 'z'] i 'b'"
 
 
 parseCharP1 :: TestTree
 parseCharP1 = testCase "parse charP \'z\'" $ assertEqual [] (Chr 'z') val
-    where (val, _) = head $ parse charP "\'z\'"
+  where (val, _) = head $ parse charP "\'z\'"
 
 parseCharP2 :: TestTree
 parseCharP2 = testCase "parse charP \'$\'" $ assertEqual [] (Chr '$') val
-    where (val, _) = head $ parse charP "\'$\'"
+  where (val, _) = head $ parse charP "\'$\'"
 
 
 parseQuotedStringP1 :: TestTree
 parseQuotedStringP1 = testCase "parse parsequotedStringP \"abc\""
-    $ assertEqual [] (Str "abc") val
-    where (val, _) = head $ parse quotedStringP "\"abc\""
+  $ assertEqual [] (Str "abc") val
+  where (val, _) = head $ parse quotedStringP "\"abc\""
 
 parseQuotedStringP2 :: TestTree
 parseQuotedStringP2 = testCase "parse parsequotedStringP \"\""
-    $ assertEqual [] (Str "") val
-    where (val, _) = head $ parse quotedStringP "\"\""
+  $ assertEqual [] (Str "") val
+  where (val, _) = head $ parse quotedStringP "\"\""
 
 formatStack1 :: TestTree
 formatStack1 = testCase "formatStack after running"
-    $ assertEqual [] ["[ 1.100000 1.100000 ]", "[ 1.100000 1.100000 ]"] val
-    where val = formatStack $ stack $ runQuot sKeep01
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  $ assertEqual [] ["[ 1.100000 1.100000 ]", "[ 1.100000 1.100000 ]"] val
+  where val = formatStack $ stack $ runQuot sKeep01
+
+escapeNewLine1 :: TestTree
+escapeNewLine1 = testCase "escapeNewLine parser test"
+  $ assertEqual [] ["a","","z"] val
+  where
+    val = result res
+    res = runQuot sKeep02
 
