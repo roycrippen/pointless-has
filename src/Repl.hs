e84dad7083bbe454b6863070cab8a45ca2cf1ad0
roycrippen <roycrippen@gmail.com>
2017-12-16 16:19:58 -0600

    split out repl

---
+++ src/Repl.hs
@@ -0,0 +1,84 @@
+module Repl (startRepl) where
+
+import           Control.Monad   (forever)
+import           CoreLibrary     (coreDefinitions, getQuotations)
+import qualified Data.Map        as M (fromList)
+import           Interpreter     (Lang (..), runQuotation)
+import           Parser          (parse)
+import           PointlessParser (nakedQuotations)
+import           Primitives      (primitives)
+import           System.Exit     (exitSuccess)
+import           System.IO       (hFlush, stdout)
+
+startRepl :: IO ()
+startRepl = do
+  putStrLn "Welcome to the Pointless repl"
+  putStrLn "Enter a valid Pointless expression or :h for help"
+  putStrLn "Pointless> "
+  let defs = getQuotations coreDefinitions ++ primitives
+      lang = Lang (M.fromList defs) [] [] ""
+  runPointless lang
+
+runQuot :: String -> Lang -> Lang
+runQuot s = runQuotation qs
+ where (qs, _):_ = parse nakedQuotations s
+
+runPointless :: Lang -> IO ()
+runPointless lang = forever $ do
+  putStr "Pointless> "
+  hFlush stdout
+  quoteStr' <- getLine
+  -- putStr "quotation before = "
+  -- putStrLn quoteStr'
+  let quoteStr = replaceStr "\\n" "\n" quoteStr'
+  runCommand quoteStr
+    where
+      runCommand :: String -> IO ()
+      runCommand s =
+        case take 2 s of
+        ":q" -> exitSuccess
+        ":h" -> showHelp >> runPointless lang
+        ":l" -> do
+          lang' <- loadAndRunFile s lang
+          runPointless lang' { result = [] }
+        ":r" -> showHelp >> runPointless lang
+        _    -> do
+          let lang' = runQuot s lang
+          -- mapM_ putStrLn (errors lang')
+          mapM_ putStrLn (result lang')
+          runPointless $ lang' { result = [] }
+
+loadAndRunFile :: String -> Lang -> IO Lang
+loadAndRunFile file lang = do
+  let file' = replaceStr ":l " "" file ++ ".pless"
+  source' <- readFile file'
+  let source = replaceStr  "\\n" "\n" source'
+      lang' = runQuot source lang
+  -- mapM_ putStrLn (errors lang')
+  mapM_ putStrLn (result lang')
+  return lang'
+
+showHelp :: IO ()
+showHelp = do
+  putStrLn ":h             show help"
+  putStrLn ":q             exit"
+  putStrLn "Ctrl+c         exit"
+  putStrLn ":l <filename>  load a Pointless script"
+  putStrLn ":r             reload Pointless script"
+  putStrLn "or enter a valid Pointless expression"
+  putStrLn "Pointless> "
+
+replaceStr :: String -> String -> String -> String
+replaceStr _ _ [] = []
+replaceStr old new str = go str
+  where
+    go [] = []
+    go str'@(x:xs) =
+      let (prefix, rest) = splitAt n str'
+      in
+        if old == prefix
+        then new ++ go rest
+        else x : go xs
+    n = length old
+
+
