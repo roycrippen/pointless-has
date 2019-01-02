f4c0f712e7d5341430a0e11fb191baaac2deaa74
roy.crippen <roy.crippen@archarithms.com>
2019-01-02 08:38:21 -0600

    linting and formating

--- src/Core.hs
+++ src/Core.hs
@@ -1,9 +1,17 @@
-module Core ( coreDefinitions ) where
+module Core
+  ( coreDefinitions
+  )
+where
 
-import qualified Data.Map        as M (Map, fromList)
-import           Interpreter     (WordP (..), Vocabulary)
-import           Parser          (parse)
-import           PointlessParser (nakedQuotations)
+import qualified Data.Map                      as M
+                                                ( Map
+                                                , fromList
+                                                )
+import           Interpreter                    ( WordP(..)
+                                                , Vocabulary
+                                                )
+import           Parser                         ( parse )
+import           PointlessParser                ( nakedQuotations )
 import           Primitives
 
 coreDefinitions :: Vocabulary
@@ -11,7 +19,7 @@ coreDefinitions = M.fromList $ getQuotations coreLibrary ++ primitives
 
 getQuotation :: (String, String) -> (String, WordP)
 getQuotation (name, qs) = (name, Quotation q)
-  where (q, _):_ = parse nakedQuotations qs
+  where (q, _) : _ = parse nakedQuotations qs
 
 getQuotations :: [(String, String)] -> [(String, WordP)]
 getQuotations = map getQuotation
@@ -86,21 +94,23 @@ coreLibrary =
   , ("dip1"    , "dip")
   , ("dip2"    , "[dip] cons dip")
   , ("dip3"    , "[dip] cons [dip] cons dip")
-  , ("nullary" , "stack [exec] dip cons unstack swap pop")
-  , ("unary"   , "stack [exec] dip cons unstack [pop2] dip")
-  , ("binary"  , "stack [exec] dip cons unstack [pop3] dip")
+  , ("nullary", "stack [exec] dip cons unstack swap pop")
+  , ("unary", "stack [exec] dip cons unstack [pop2] dip")
+  , ("binary", "stack [exec] dip cons unstack [pop3] dip")
   , ("exec2"   , "[dip] dip exec")
   , ("unary2"  , "[unary  ] cons dup exec2")
-  , ("infra"   , "swons [stack] dip cons unstack [exec stack] dip cons unstack")
+  , ("infra", "swons [stack] dip cons unstack [exec stack] dip cons unstack")
   , ("cleave"  , "[dup] dip2 swap dip2 exec")
   , ("branch"  , "[] rollup ifte")
   , ("in", "swap [=] cons filter size [1 >=] [true] [false] ifte swap pop")
-  , ("times"   , "[ pop not ] [ pop2 ] [ [ pred ] dip dup dip2 ] tailrec")
+  , ("times", "[ pop not ] [ pop2 ] [ [ pred ] dip dup dip2 ] tailrec")
   , ("repeat"  , "[swons] cons [] rollup times")
-  , ("step"    , "[null2] [pop2] [[uncons] dip dup dip2] tailrec")
+  , ( "step"
+    , "[null2] [pop2] [[uncons] dip dup dip2] tailrec"
+    )
   -- , ("size"    , "0 swap [pop succ] step")
   , ("map", "swap [[]] [\"\"] iflist swap rolldown [swons] concat step reverse")
-  , ("fold"    , "swapd step")
+  , ("fold", "swapd step")
   , ( "filter"
     , "swap [[]] [\"\"] iflist swap rolldown [[swons] [pop] ifte] cons step reverse"
     )
@@ -157,16 +167,16 @@ coreLibrary =
   , ("uncons2" , "unconsd uncons swapd")
   , ("swons2"  , "swapd swons swonsd")
   , ("unswons2", "[unswons] dip unswons swapd")
-  , ("zip"     , "[null2] [pop2 []] [uncons2] [[pairlist] dip cons] linrec")
+  , ("zip", "[null2] [pop2 []] [uncons2] [[pairlist] dip cons] linrec")
   , ( "from-to"
     , "unitlist [pop pop] swoncat [>] swap [[dup succ] dip] [cons] linrec"
     )
   , ("from-to-list"  , "[] from-to")
   , ("from-to-string", "\"\" from-to")
   , ("tailrec"       , "[] linrec")
-  , ("split"         , "dup2 filter rollup [ not ] concat filter")
-  , ("pairstep"      , "[dupd] swoncat [step pop] cons cons step")
-  , ("mapr"          , "[[null] [] [uncons]] dip [dip cons] cons linrec")
+  , ("split", "dup2 filter rollup [ not ] concat filter")
+  , ("pairstep", "[dupd] swoncat [step pop] cons cons step")
+  , ("mapr", "[[null] [] [uncons]] dip [dip cons] cons linrec")
   , ("foldr", "[[[null]] dip unitlist [pop] swoncat [uncons]] dip linrec")
   , ( "stepr2"
     , "[[null2] [pop pop]] dip [dip] cons [dip] cons [uncons2] swoncat tailrec"
@@ -181,15 +191,15 @@ coreLibrary =
   , ( "variance"
     , "0.0 swap dup [sum] [size] cleave dup [/ [- dup * +] cons step] dip pred /"
     )
-  , ("while"     , "swap [not] concat [] rolldown tailrec")
+  , ("while", "swap [not] concat [] rolldown tailrec")
   , ("to-upper"  , "['a' >= ] [32 -] when")
   , ("to-lower"  , "['a' <  ] [32 +] when")
   , ("positive"  , "0 >")
   , ("negative"  , "0 <")
   , ("prime", "2 [[dup * >] nullary [rem 0 >] dip and] [succ] while dup * < ")
-  , ("fact"      , "[1 1] dip [dup [*] dip succ] times pop")
-  , ("fib"       , "[1 0] dip [swap [+] unary] times popd")
-  , ("nfib"      , "[1 1] dip [dup [+ succ] dip swap] times pop")
+  , ("fact", "[1 1] dip [dup [*] dip succ] times pop")
+  , ("fib", "[1 0] dip [swap [+] unary] times popd")
+  , ("nfib", "[1 1] dip [dup [+ succ] dip swap] times pop")
   , ("gcd"       , "[0 >] [dup rollup rem] while pop")
   , ("fahrenheit", "9 * 5 / 32 + ")
   , ("celsius"   , "32 - 5 * 9 /")
@@ -205,12 +215,14 @@ coreLibrary =
   , ("putchars"    , "[putch] step")
   , ("putstrings"  , "[putchars] step")
   , ("current-path", " \"\" ")
-  , ("libload"     , "current-path swap concat \".pless\" concat _libopen")
-  , ("run-tests"   , "current-path swap concat \".pless\" concat _runtests")
+  , ("libload", "current-path swap concat \".pless\" concat _libopen")
+  , ("run-tests", "current-path swap concat \".pless\" concat _runtests")
   , ( "s-to-list"
     , "dup size [uncons] times pop stack dup size 1 - [rolldown pop] times swap pop reverse"
     )
-  , ("unlist", "dup size dup \"_sizeUnlist\" set-var [ uncons ] times pop _sizeUnlist")
+  , ( "unlist"
+    , "dup size dup \"_sizeUnlist\" set-var [ uncons ] times pop _sizeUnlist"
+    )
   , ( "_flatten"
     , "dup size \"_sizeFlatten\" set-var [ [ unlist ] [ 1 ] iflist ] step [ ] _sizeFlatten [ swap [ cons ] times ] times"
     )
